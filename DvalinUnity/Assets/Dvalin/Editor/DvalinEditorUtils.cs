42373430c5e1c2e81d694a6513edf3f235b38d7e
Henrik Melsom <melsom.henrik@gmail.com>
2021-02-18 18:31:52 +0100

    added readme and switched to vscode

--- DvalinUnity/Assets/Dvalin/Editor/DvalinEditorUtils.cs
+++ DvalinUnity/Assets/Dvalin/Editor/DvalinEditorUtils.cs
@@ -1,11 +1,25 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 using UnityEditor;
 
-public class DvalinEditorUtils : MonoBehaviour
+public class DvalinEditorUtils
 {
     [MenuItem("Dvalin/Build Asset Bundles")]
     static void BuildAssetBundles()
     {
-        BuildPipeline.BuildAssetBundles("AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        string stageDirName = "AssetBundles";
+        string projectRootDir = Path.Combine(Application.dataPath, "..");
+        string stagePath = Path.Combine(projectRootDir, stageDirName);
+        string distDir = Path.Combine(projectRootDir, @"..\dist");
+
+        BuildPipeline.BuildAssetBundles(stagePath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+
+        foreach (var file in Directory.EnumerateFiles(stagePath))
+        {
+            if (file.EndsWith(".manifest") || file.EndsWith(stageDirName)) continue;
+
+            string fileName = Path.GetFileName(file);
+            File.Copy(file, Path.Combine(distDir, fileName), true);
+        }
     }
 }
